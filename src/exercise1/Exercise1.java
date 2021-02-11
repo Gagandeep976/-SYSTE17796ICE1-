dee0ce32dc934e0aca74f58c37d1ee2d175d49b4
SAMSUNG <SAMSUNG@DESKTOP-3486K56>
2021-02-12 01:12:36 +0530

    My first exercise.

---
+++ src/exercise1/Exercise1.java
@@ -0,0 +1,39 @@
+/*
+ * Gagandeep Singh
+ * Student ID: 991616664
+ * SYST10199 - Web Programming
+ */
+package exercise1;
+
+import java.util.Scanner;
+public class Exercise1 {
+
+    public static void main(String[] args) {
+       int []temps;
+       temps=new int[14];
+       Scanner in = new Scanner(System.in);
+       System.out.println("Enter the 14 temperatures collected in one week");
+       for(int i=0;i<temps.length;i++){
+             temps[i]=in.nextInt();
+       }
+       System.out.println(calcAvg(temps));
+    }
+    
+    
+    static int calcAvg(int []temps){
+        int num=0;
+        int total=0;
+        for(int i=0;i<temps.length;i++){
+            if(temps[i]<50 && temps[i]>-50){
+                total=total+temps[i];
+                num++;
+            }
+        }
+        int average=total/num;
+        System.out.println("Sum of all valid temperatures: "+total);
+        System.out.println("Number of accepted temperatures: "+num);
+        System.out.println("Average is:");
+        return average;
+    }
+    
+}
